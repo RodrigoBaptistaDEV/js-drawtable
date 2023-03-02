0fcecee51c0d086f8835e3e20b01bce77bbdd5a5
Rodrigo Baptista <100215559+RodrigoBaptistaDEV@users.noreply.github.com>
2023-03-02 14:07:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let currentColor = 'black';
+let screen = document.querySelector('#tela');
+let ctx = screen.getContext('2d');
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
+
+document.querySelectorAll('.colorArea .color').forEach(item => {
+    item.addEventListener('click', colorClickEvent);
+});
+screen.addEventListener('mousedown', mouseDownF)
+screen.addEventListener('mousemove', mouseMoveF)
+screen.addEventListener('mouseup', mouseUpF)
+
+function colorClickEvent(e) {
+    let color = e.target.getAttribute("data-color")
+    currentColor = color
+
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active')
+}
+function mouseDownF(e) {
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+
+}
+function mouseMoveF(e) {
+    if (canDraw) {
+        Draw(e.pageX, e.pageY)
+    }
+}
+function mouseUpF() {
+    canDraw = false;
+}
+function Draw(x, y) {
+    pointX = x - screen.offsetLeft;
+    pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = 'round';
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+function clearScreen() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas, width, ctx.canvas.height);
+}
